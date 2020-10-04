1ef56142378ea2bb58af35a36ec61a7a4a319839
scwindy <scwyau@connect.ust.hk>
2020-10-04 17:29:30 +0800

    Initial Commit!

---
+++ src/main/java/Library.java
@@ -0,0 +1,6 @@
+public class Library {
+    public static void main(String arg[]) {
+        comp3111.lab3.ex2.UIApplication.run(arg);
+    }
+
+}
---
+++ src/main/java/comp3111/lab3/ex2/Lab3Controller.java
@@ -0,0 +1,24 @@
+package comp3111.lab3.ex2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Lab3Controller {
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    private Label label1;
+
+    @FXML
+    private TextField TextField1;
+
+    @FXML
+    void buttonPressed(ActionEvent event) {
+        label1.setText(TextField1.getText());
+    }
+}
